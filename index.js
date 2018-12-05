f50702af11c054954b52bebe567ffaa8a4aee98e
Sean Swan <ogpswan@YAHOO.COM>
2018-12-05 15:45:07 -0800

    finishingtemplate for card and overview setting up callbacks

--- index.js
+++ index.js
@@ -9,31 +9,41 @@ const server = http.createServer((req, res) => {
 
   
   const pathName = url.parse(req.url, true).pathname;  
-    
-  
+    const id = url.parse(req.url, true).query.id;
+
+  // PRODUCT OVERVIEW
   if (pathName === '/packages' || pathName === '/') {
     res.writeHead(200, { 'Content-type': 'text/html'});
-    res.end('this is the PACKAGES page');
+
+    fs.readFile(`${__dirname}/templates/overview.html`, 'utf-8', (err, data) => {
+      
+     
+
+      fs.readFile(`${__dirname}/templates/card.html`, 'utf-8', (err, data) => {
+      
+        res.end(data);
+      });
+    });
+   
+
+
+
+
 }
 
+// LAPTOP DETAIL
 else if (pathName === '/details' && id < packageData.length)  {
   res.writeHead(200, { 'Content-type': 'text/html'});
   
-  fs.readFile(`${__dirname}/package.html`, 'utf-8', (err, data) => {
+  fs.readFile(`${__dirname}/templates/packages.html`, 'utf-8', (err, data) => {
     const package = packageData[id];
-    let output = data.replace(/{%PRODUCTNAME%}/g, packageData[id].productName);
-     output = output.replace(/{%IMAGE%}/g, package.image);
-     output = output.replace(/{%PRICE%}/g, package.price);
-     output = output.replace(/{%TWELVEMONTH-TWO%}/g, package.twelvemonth-two);
-     output = output.replace(/{%TWELVEMONTH-THREE%}/g, package.twelvemonth-three);
-     output = output.replace(/{%TWELVEMONTH-FOUR%}/g, package.twelvemonth-four);
-     output = output.replace(/{%TWELVEMONTH-FIVE%}/g, package.twelvemonth-five);
-     output = output.replace(/{%DESCRIPTION%}/g, package.description);
+    const output = replaceTemplate(data, package);
     res.end(output);
   });
 
 }
 
+// URL NOT FOUND
 else {
   res.writeHead(404, { 'Content-type': 'text/html'});
   res.end('URL was not found on the server!');
@@ -55,7 +65,18 @@ server.listen(1337, '127.0.0.1', () =>  {
 
 
 
-
+function replaceTemplate(originalHtml, package) {
+  let output = original.replace(/{%PRODUCTNAME%}/g, packageData[id].productName);
+  output = output.replace(/{%IMAGE%}/g, package.image);
+  output = output.replace(/{%PRICE%}/g, package.price);
+  output = output.replace(/{%TWELVEMONTH-TWO%}/g, package.twelvemonth-two);
+  output = output.replace(/{%TWELVEMONTH-THREE%}/g, package.twelvemonth-three);
+  output = output.replace(/{%TWELVEMONTH-FOUR%}/g, package.twelvemonth-four);
+  output = output.replace(/{%TWELVEMONTH-FIVE%}/g, package.twelvemonth-five);
+  output = output.replace(/{%DESCRIPTION%}/g, package.description);
+  output = output.replace(/{%ID%}/g, package.id);
+  return output;
+}
 
 /*const query = url.parse(req.url, true)
   console.log(query);*/
